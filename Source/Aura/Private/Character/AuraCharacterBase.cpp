12893d2a812f897f3119d8cb6e92b834ebd5ac7a
unknown <sgbeng@126.com>
2026-01-28 10:51:08 +0800

    The Enemy Character

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright Druid SSS
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+}
+
